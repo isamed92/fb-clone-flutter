b37bf8098bf11300a80c21455b721151f42f3f71
Isaias Medina <jose.i.medina@set.ypf.com>
2024-08-24 20:56:46 -0300

    chore: initial commit

---
+++ android/app/src/main/kotlin/com/isa/caralibro/caralibro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.isa.caralibro.caralibro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:caralibro/app/app.dart';
+import 'package:caralibro/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
